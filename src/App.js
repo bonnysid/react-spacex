24fbd963c2d9c478cde7ec782af87806330efb3d
bonnysid <bonnysidworker@gmail.com>
2020-10-13 23:40:06 +0400

    add some components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import React from 'react';
+import './style.css';
+import Header from './components/Header';
+import Main from './components/Main';
+import Features from './components/Features';
+import Footer from './components/Footer';
+import Calendar from './components/Calendar';
+import Details from './components/Details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Features />
+      <Footer />
+    </>
   );
 }
 
