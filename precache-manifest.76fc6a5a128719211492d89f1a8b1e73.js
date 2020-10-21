22af1b73c8f579445d2d71beb3d53411a099af0c
bonnysid <bonnysidworker@gmail.com>
2020-10-22 00:55:50 +0400

    Updates

---
+++ precache-manifest.76fc6a5a128719211492d89f1a8b1e73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e0747d80bb20a3b029c8ab5a56f0691",
+    "url": "/react-spacex/index.html"
+  },
+  {
+    "revision": "866b849c86d6b8d17c48",
+    "url": "/react-spacex/static/css/main.241cfcbc.chunk.css"
+  },
+  {
+    "revision": "66a4eb6fb19350622c35",
+    "url": "/react-spacex/static/js/2.d5100ef5.chunk.js"
+  },
+  {
+    "revision": "8a9adb48f7af2062183ac70b4922c4ef",
+    "url": "/react-spacex/static/js/2.d5100ef5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "866b849c86d6b8d17c48",
+    "url": "/react-spacex/static/js/main.d4ce158e.chunk.js"
+  },
+  {
+    "revision": "09d8ec1f06b269832a15",
+    "url": "/react-spacex/static/js/runtime-main.1db52588.js"
+  },
+  {
+    "revision": "d5ab31ce033fc418fd1af87f7b934e1b",
+    "url": "/react-spacex/static/media/logo.d5ab31ce.svg"
+  }
+]);
